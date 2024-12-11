66ab78e20e4914f5a029e6805096685ace56f532
Mateo Besednik <mateo.besednik@gmail.com>
2024-12-11 09:28:23 +0100

    Implemented React Router, Added routes for Home, Dashboard, Asset and Report page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import "./App.css";
+// import Login from "./components/Login/Login";
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       {/* <Login /> */}
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Home />} />
+//             <Route path="blogs" element={<Blogs />} />
+//             <Route path="contact" element={<Contact />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
 import "./App.css";
-import Login from "./components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+import AssetPage from "./pages/AssetPage";
+import ReportPage from "./pages/ReportPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Login />
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/asset" element={<AssetPage />} />
+        <Route path="/report" element={<ReportPage />} />
+      </Routes>
+    </div>
   );
 }
 
