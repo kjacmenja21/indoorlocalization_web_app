e822bf6bfe01bae2f67d386cab89b2e1c933d820
Mateo Besednik <mateo.besednik@gmail.com>
2024-12-13 08:39:00 +0100

    Apply SCSS with BEM for responsive table design on AssetTable Component

--- src/components/AssetTable/AssetTable.jsx
+++ src/components/AssetTable/AssetTable.jsx
@@ -27,50 +27,130 @@ const AssetTable = ({ assets, onNavigate }) => {
   };
 
   return (
-    <table style={{ borderCollapse: "collapse", width: "100%" }}>
-      <thead>
-        <tr>
-          <th style={styles.header}>ID</th>
-          <th style={styles.header}>Name</th>
-          <th style={styles.header}>X</th>
-          <th style={styles.header}>Y</th>
-          <th style={styles.header}>Last Sync</th>
-          <th style={styles.header}>Floor Map Name</th>
-          <th style={styles.header}>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {assets.length > 0 ? (
+    <>
+      <ul className="responsive-table__list">
+        <li className="responsive-table__list-item responsive-table__list-item--header">
+          <div className="responsive-table__column responsive-table__column--1">
+            ID
+          </div>
+          <div className="responsive-table__column responsive-table__column--2">
+            Name
+          </div>
+          <div className="responsive-table__column responsive-table__column--3">
+            X
+          </div>
+          <div className="responsive-table__column responsive-table__column--4">
+            Y
+          </div>
+          <div className="responsive-table__column responsive-table__column--4">
+            Floor Map Name
+          </div>
+          <div className="responsive-table__column responsive-table__column--4">
+            Details
+          </div>
+        </li>
+        {assets.length > 0 &&
           assets.map((asset) => (
-            <tr key={asset.id}>
-              <td style={styles.cell}>{asset.id}</td>
-              <td style={styles.cell}>{asset.name}</td>
-              <td style={styles.cell}>{asset.x}</td>
-              <td style={styles.cell}>{asset.y}</td>
-              <td style={styles.cell}>
+            <li
+              key={asset.id}
+              className="responsive-table__list-item responsive-table__list-item--row"
+            >
+              <div
+                className="responsive-table__column responsive-table__column--1"
+                data-label="ID"
+              >
+                {asset.id}
+              </div>
+              <div
+                className="responsive-table__column responsive-table__column--1"
+                data-label="Name"
+              >
+                {asset.name}
+              </div>
+              <div
+                className="responsive-table__column responsive-table__column--2"
+                data-label="X"
+              >
+                {asset.x}
+              </div>
+              <div
+                className="responsive-table__column responsive-table__column--3"
+                data-label="Y"
+              >
+                {asset.y}
+              </div>
+              <div
+                className="responsive-table__column responsive-table__column--4"
+                data-label="Last sync"
+              >
                 {new Date(asset.lastSync).toLocaleString()}
-              </td>
-              <td style={styles.cell}>{asset.floorMapName}</td>
-              <td style={styles.cell}>
+              </div>
+              <div
+                className="responsive-table__column responsive-table__column--4"
+                data-label="Floor Map Name"
+              >
+                {asset.floorMapName}
+              </div>
+              <div
+                className="responsive-table__column responsive-table__column--4"
+                data-label="Details"
+              >
                 <button
                   style={styles.iconButton}
                   onClick={() => onNavigate(asset.id)}
                 >
                   🔍
                 </button>
-              </td>
-            </tr>
-          ))
-        ) : (
-          <tr>
-            <td colSpan="7" style={styles.noData}>
-              No assets found.
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </>
   );
+
+  // <table style={{ borderCollapse: "collapse", width: "100%" }}>
+  //   <thead>
+  //     <tr>
+  //       <th style={styles.header}>ID</th>
+  //       <th style={styles.header}>Name</th>
+  //       <th style={styles.header}>X</th>
+  //       <th style={styles.header}>Y</th>
+  //       <th style={styles.header}>Last Sync</th>
+  //       <th style={styles.header}>Floor Map Name</th>
+  //       <th style={styles.header}>Actions</th>
+  //     </tr>
+  //   </thead>
+  //   <tbody>
+  //     {assets.length > 0 ? (
+  //       assets.map((asset) => (
+  //         <tr key={asset.id}>
+  //           <td style={styles.cell}>{asset.id}</td>
+  //           <td style={styles.cell}>{asset.name}</td>
+  //           <td style={styles.cell}>{asset.x}</td>
+  //           <td style={styles.cell}>{asset.y}</td>
+  //           <td style={styles.cell}>
+  //             {new Date(asset.lastSync).toLocaleString()}
+  //           </td>
+  //           <td style={styles.cell}>{asset.floorMapName}</td>
+  //           <td style={styles.cell}>
+  //             <button
+  //               style={styles.iconButton}
+  //               onClick={() => onNavigate(asset.id)}
+  //             >
+  //               🔍
+  //             </button>
+  //           </td>
+  //         </tr>
+  //       ))
+  //     ) : (
+  //       <tr>
+  //         <td colSpan="7" style={styles.noData}>
+  //           No assets found.
+  //         </td>
+  //       </tr>
+  //     )}
+  //   </tbody>
+  // </table>
 };
 
 export default AssetTable;
