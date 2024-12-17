db98825331a0bb0da9fc9d0170fe3af7916d78b0
Mateo Besednik <mateo.besednik@gmail.com>
2024-12-17 01:19:18 +0100

    Change react icon in asset table

--- src/components/AssetTable/AssetTable.jsx
+++ src/components/AssetTable/AssetTable.jsx
@@ -1,23 +1,9 @@
 import React from "react";
+import { FaInfoCircle } from "react-icons/fa";
 import "./_assetTable.scss";
 
 const AssetTable = ({ assets, onNavigate }) => {
   const styles = {
-    header: {
-      border: "1px solid #ddd",
-      padding: "8px",
-      backgroundColor: "#f4f4f4",
-      textAlign: "left",
-    },
-    cell: {
-      border: "1px solid #ddd",
-      padding: "8px",
-    },
-    noData: {
-      textAlign: "center",
-      padding: "16px",
-      backgroundColor: "#f9f9f9",
-    },
     iconButton: {
       background: "none",
       border: "none",
@@ -102,7 +88,7 @@ const AssetTable = ({ assets, onNavigate }) => {
                   style={styles.iconButton}
                   onClick={() => onNavigate(asset.id)}
                 >
-                  🔍
+                  <FaInfoCircle size={23} />
                 </button>
               </div>
             </li>
@@ -110,50 +96,6 @@ const AssetTable = ({ assets, onNavigate }) => {
       </ul>
     </>
   );
-
-  // <table style={{ borderCollapse: "collapse", width: "100%" }}>
-  //   <thead>
-  //     <tr>
-  //       <th style={styles.header}>ID</th>
-  //       <th style={styles.header}>Name</th>
-  //       <th style={styles.header}>X</th>
-  //       <th style={styles.header}>Y</th>
-  //       <th style={styles.header}>Last Sync</th>
-  //       <th style={styles.header}>Floor Map Name</th>
-  //       <th style={styles.header}>Actions</th>
-  //     </tr>
-  //   </thead>
-  //   <tbody>
-  //     {assets.length > 0 ? (
-  //       assets.map((asset) => (
-  //         <tr key={asset.id}>
-  //           <td style={styles.cell}>{asset.id}</td>
-  //           <td style={styles.cell}>{asset.name}</td>
-  //           <td style={styles.cell}>{asset.x}</td>
-  //           <td style={styles.cell}>{asset.y}</td>
-  //           <td style={styles.cell}>
-  //             {new Date(asset.lastSync).toLocaleString()}
-  //           </td>
-  //           <td style={styles.cell}>{asset.floorMapName}</td>
-  //           <td style={styles.cell}>
-  //             <button
-  //               style={styles.iconButton}
-  //               onClick={() => onNavigate(asset.id)}
-  //             >
-  //               🔍
-  //             </button>
-  //           </td>
-  //         </tr>
-  //       ))
-  //     ) : (
-  //       <tr>
-  //         <td colSpan="7" style={styles.noData}>
-  //           No assets found.
-  //         </td>
-  //       </tr>
-  //     )}
-  //   </tbody>
-  // </table>
 };
 
 export default AssetTable;
